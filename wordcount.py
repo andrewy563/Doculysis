92993002563f2b43a8159c6b31ba9016e3cb7dea
Andrew Yoon <andrewyoon@Andrews-MacBook-Pro.local>
2017-09-19 16:42:46 -0700

    lists most frequent words

--- wordcount.py
+++ wordcount.py
@@ -1,9 +1,10 @@
 import sys
+from collections import OrderedDict
 
 # This script is used for my personal reports
 
 fo = open(sys.argv[1], 'r')
-
+count = int(sys.argv[2])
 tel = {}
 
 with fo as f:
@@ -16,10 +17,14 @@ with fo as f:
 
 # def wordCount(number):
 
+def wordCount(file, input):
+	tel_sorted_by_value = OrderedDict(sorted(file.items(), key = lambda x: x[1], reverse=True))
+	for x in xrange(input):
+		print tel_sorted_by_value.popitem(last=False)
 
 if  __name__ == "__main__":
 	print "This is the name of the script", sys.argv[0]
 	print "This is the name of the file", sys.argv[1]
 	print "How many words to view?", sys.argv[2]
 	print tel
-	print sorted(tel.values())
+	wordCount(tel, count)
